1864cb3ac0efe3e6f40eafdbe8624097bfbea590
Daniel Savage <dansvg@gmail.com>
2013-11-22 14:19:22 -0800

    I’m a bit of a dick.

--- OpenCL-Experiment/OpenCL-Experiment/main.cpp
+++ OpenCL-Experiment/OpenCL-Experiment/main.cpp
@@ -6,13 +6,13 @@
 //  Copyright (c) 2013 FrostTree Games. All rights reserved.
 //
 
-#include <iostream>
+#include <cstdio>
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    std::cout << "Hello, World!\n";
+    printf("Hello, World!\n");
     return 0;
 }
 
