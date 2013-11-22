1b792e843529fef85c5f00506422f6c1bc1cbd7d
Daniel Savage <dansvg@gmail.com>
2013-11-22 14:18:17 -0800

    There we go!

---
+++ OpenCL-Experiment/OpenCL-Experiment/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenCL-Experiment
+//
+//  Created by Daniel Savage on 11/22/2013.
+//  Copyright (c) 2013 FrostTree Games. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
